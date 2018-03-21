54a9d2fa0b349665625ea2293bb42f408ecebe0f
Takashi Kokubun <takashikkbn@gmail.com>
2018-03-21 14:46:46 +0900

    Version 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+# v0.1.1
+
+* Show running job
+* Change bar colors
+* Change font sizes
+
 # v0.1.0
 
 * Initial release
--- lib/benchmark_driver/output/gruff/version.rb
+++ lib/benchmark_driver/output/gruff/version.rb
@@ -1,7 +1,7 @@
 module BenchmarkDriver
   module Output
     class Gruff
-      VERSION = "0.1.0"
+      VERSION = "0.1.1"
     end
   end
 end
